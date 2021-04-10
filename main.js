16b99805293d67e12e954900275f4cc384705385
OM Miles <olivia.m.miles@gmail.com>
2021-04-10 16:49:37 -0700

    Tidy up JS code

--- main.js
+++ main.js
@@ -1,49 +1,4 @@
-// const endangered_url = `https://search.idigbio.org/v2/search/records/?rq={%22data.dwc:datasetName%22:%22Endangered%22}`
-
-// const stateInput = document.querySelector('.search-input')
-
-// https://search.idigbio.org/v2/search/records/?rq=%7B%22data.dwc:datasetName%22%3A+%22Endangered%22%7D
-
-// const state_url = `https://search.idigbio.org/v2/search/records/?rq={%22data.dwc:stateProvince%22:%22Kansas%22}`
-
-// https://search.idigbio.org/v2/search/records/?rq={%22data.dwc:datasetName%22:%22Endangered%22}&{%22data.dwc:stateProvince%22:%22${searchTerm}%22}
-
-// async function fetchEndangered() {
-
-//     const response = await fetch(endangered_url)
-//     const results = await response.json()
-//     // JS destructuring
-//     // we store the results' JSON object items into a variable
-//     const {items} = results
-//     // console.log(results)
-
-//     items.forEach(item =>{
-//         const ul = document.querySelector(`ul`)
-//         let li = document.createElement('li')
-//         li.innerHTML = item.indexTerms.specificepithet
-//         ul.appendChild(li)
-//         console.log(li)
-//     })
-
-//     items.forEach(item =>{
-//         console.log(item.indexTerms.stateprovince)
-//     })
-// }
-
 const inputEl = document.querySelector(`input`)
-
-// const doSearch = event => {
-//     // search hint function
-
-//     const searchTerm = input.value
-//     if (event.key === `Enter` && searchTerm.length > 2 && searchTerm.length < 13) {
-//         console.log(`Search for`, searchTerm)
-//     }
-//     const endangered_state_url = `https://search.idigbio.org/v2/search/records/?rq={%22data.dwc:stateProvince%22:%22${searchTerm}%22}`
-//     return endangered_state_url
-// }
-
-// input.addEventListener(`keyup`, doSearch)
 
 async function doSearch(event) {
 
@@ -77,8 +32,3 @@ async function doSearch(event) {
 }
 
 inputEl.addEventListener(`keyup`, doSearch)
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     // fetchEndangered()
-//     fetchStateEndangered()
-// }); 
